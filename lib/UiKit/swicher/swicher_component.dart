644955231b8d1558b4276cc8845c17698682f0e9
Mohamaddanialbatmany <koorsna@gmail.com>
2020-03-13 20:18:00 +0330

    start swicher

---
+++ lib/UiKit/swicher/swicher_component.dart
@@ -0,0 +1,12 @@
+import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+
+@Component(
+  selector: 's-w',
+  templateUrl: 'swicher_component.html',
+  styleUrls: ['swicher_component.css'],
+  directives: [formDirectives, coreDirectives],
+)
+class Swichers{
+
+}
