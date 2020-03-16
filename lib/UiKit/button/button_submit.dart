76271092d065a448293e14b0d19a3f81969d5380
Mohamaddanialbatmany <koorsna@gmail.com>
2020-03-16 12:29:29 +0330

    button starter

--- lib/UiKit/button/button_submit.dart
+++ lib/UiKit/button/button_submit.dart
@@ -6,7 +6,7 @@ import 'package:angular_forms/angular_forms.dart';
 @Component(
     selector: 'submit-btn',
     template: '''
-              <button class="button">Send</button>
+              <button class="btnSubmit">Submit</button>
               ''',
     styleUrls: ['buttons_component.css'],
     directives: [formDirectives, coreDirectives]
